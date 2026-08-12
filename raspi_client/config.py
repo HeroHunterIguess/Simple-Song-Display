81b13024d71d89ce63e066b7a8dfd79e64dc663d
Raylee <abramscarter1999@gmail.com>
2026-08-12 11:14:02 -0400

    Create config.py

---
+++ raspi_client/config.py
@@ -0,0 +1,31 @@
+# Simple Song Display configuration
+# Default config
+# Do not remove any values
+
+
+#############################
+### Appearance            ###
+#############################
+
+background_color = (30,30,30)
+
+display_size = (340,100)
+
+text_color = (255,255,255)
+font = "Monospace"
+font_size = 20
+line_padding = 5
+horizontal_padding = 8
+
+mode = "Full" # Currently 'Full' is the only mode
+
+album_cover_size = (72,72)
+
+
+#############################
+### Misc                  ###
+#############################
+
+data_file_location = "/home/herohunter/misc/song_info.txt"
+
+truncation_length = 10 # Length of a line of text before its shortened
