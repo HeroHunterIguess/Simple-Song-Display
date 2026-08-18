37fb3d24ede847e255ce95c2af8ec3e2dfb794bf
Raylee <abramscarter1999@gmail.com>
2026-08-18 13:18:48 -0400

    update pause icon link

--- raspi_client/config.py
+++ raspi_client/config.py
@@ -71,7 +71,7 @@ truncation_length = 10 # UNUSED # Length of a line of text before its shortened
 no_media_background_image_link = "https://c10.patreonusercontent.com/4/patreon-media/p/post/152305127/0ef2dacadccf4136bacbe9ab00e37fa6/eyJxIjoxMDAsIndlYnAiOjB9/1.jpg?token-hash=Hb8MbNOVwjsZ9YkyGhFd1sMHx-atmAJRZMMqbKtYEvY%3D&token-time=1787788800"
 no_media_message = "No Media."
 
-pause_icon = "https://static.thenounproject.com/png/409033-200.png"
+pause_icon = "https://raylees-void.neocities.org/paused.png"
 paused_darkening = 140
 
 log_file = "/home/herohunter/Everything/simple_song_display_log.txt"
